ad73bb3adfeeaab6793291605e8e39a7047513f9
Anant Jain <anant.j2409@gmail.com>
2019-12-27 17:41:39 -0500

    Revert "isolating FireBase"
    This reverts commit d3cb6d1db2c7ac88c13a47b6e338b72d823f57d0.

--- firebase.py
+++
@@ -1,6 +0,0 @@
-from firebase_admin import credentials, firestore, initialize_app
-
-# Initialize Firestore DB
-cred = credentials.Certificate('secrets/firebase_keys.json')
-default_app = initialize_app(cred)
-database = firestore.client()
