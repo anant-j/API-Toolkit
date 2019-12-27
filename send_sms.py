975442b44584d77dc5b0cf943353f3b5b166bfed
Anant Jain <anant.j2409@gmail.com>
2019-12-27 11:37:09 -0500

    Adding Maps API

--- send_sms.py
+++ send_sms.py
@@ -1,5 +1,6 @@
 from twilio.rest import Client
 import json
+import maps_api
 
 with open('twilio_keys.json') as f:
     tw_keys = json.load(f)
@@ -12,9 +13,10 @@ def send_sms(location, contact):
     account_sid = tw_keys['MY_ACCOUNT_SID']
     auth_token = tw_keys['MY_AUTH_TOKEN']
     
+    travel_time=maps_api.travel_time("229 Emerson Street, Hamilton", "33 Yonge Street, Toronto")
     # lst = ["Some", "Data"]
     # items = "\n".join(lst)
-    items=location
+    items=travel_time
     client = Client(account_sid, auth_token)
     client.messages.create(
             to = contact,
