871f2d13772521d4330045916490cd26daa64e08
Anant Jain <anant.j2409@gmail.com>
2019-12-27 23:53:12 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -8,11 +8,14 @@ with open('secrets/twilio_keys.json') as f:
 def send_sms(message_content, contact):
     account_sid = tw_keys['MY_ACCOUNT_SID']
     auth_token = tw_keys['MY_AUTH_TOKEN']
-    locations=message_decoder(message_content)
-    travel = travel_time_api.TravelTime(locations['from'],locations['to'])
-    item = travel.print_traffictime()
-    # lst = ["Some", "Data"]
-    # items = "\n".join(lst)
+    try:
+        locations=message_decoder(message_content)
+        travel = travel_time_api.TravelTime(locations['from'],locations['to'])
+        traffictime = travel.print_traffictime()
+        distance=travel.print_distance()
+        response="The distance is: "+distance+". The travel time with traffic right now is: "+traffictime
+    except:
+        response="Please format your message correctly. Type HELP for more info!"
     client = Client(account_sid, auth_token)
     client.messages.create(
         to=contact,
