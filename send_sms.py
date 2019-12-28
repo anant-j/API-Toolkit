278013dda10a9560167b5274e2fa9ff5261528c1
Anant Jain <anant.j2409@gmail.com>
2019-12-27 23:23:38 -0500

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -1,19 +1,19 @@
 from twilio.rest import Client
 import json
-import maps_api
+import travel_time_api
 with open('secrets/twilio_keys.json') as f:
     tw_keys = json.load(f)
 
 def send_sms(location, contact):
     account_sid = tw_keys['MY_ACCOUNT_SID']
     auth_token = tw_keys['MY_AUTH_TOKEN']
-    travel_time=maps_api.travel_time("229 Emerson Street, Hamilton", "33 Yonge Street, Toronto")
+    travel=travel_time_api.TravelTime("229 Emerson Street, Hamilton", "33 Yonge Street, Toronto")
+    item=travel.print_traffictime()
     # lst = ["Some", "Data"]
     # items = "\n".join(lst)
-    items=travel_time
     client = Client(account_sid, auth_token)
     client.messages.create(
             to = contact,
             from_ = tw_keys['MY_TWILIO_NUMBER'],
-            body=items
+            body=item
         )
