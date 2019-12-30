a4124a9b073cb17eb679549b209c58142c9f9516
Anant Jain <anant.j2409@gmail.com>
2019-12-30 10:18:01 -0500

    Update sms_handler.py

--- sms_handler.py
+++ sms_handler.py
@@ -2,6 +2,10 @@ from twilio.rest import Client
 import json
 import travel_time_api
 with open('secrets/twilio_keys.json') as f:
+from twilio.http.http_client import TwilioHttpClient
+
+proxy_client = TwilioHttpClient()
+proxy_client.session.proxies = {'https': os.environ['https_proxy']}
     tw_keys = json.load(f)
 
 
@@ -9,7 +13,7 @@ def send_sms(message_content, contact):
     account_sid = tw_keys['MY_ACCOUNT_SID']
     auth_token = tw_keys['MY_AUTH_TOKEN']
     if (message_content.lower().strip()=="usage"):
-        response ="\nThank you for using this service. \nPlease format your message in the following format: \n'From: Origin Location - To: Destination Location' \nThank you"        
+        response ="\nThank you for using this service. \nPlease format your message in the following format: \n'From: Origin Location - To: Destination Location' \nThank you"
     else:
         try:
             locations = message_decoder(message_content)
@@ -20,7 +24,7 @@ def send_sms(message_content, contact):
                 ". The travel time with traffic right now is: "+traffictime
         except:
             response = "Please format your message correctly. Type usage for more info!"
-    client = Client(account_sid, auth_token)
+    client = Client(account_sid, auth_token, http_client=proxy_client)
     client.messages.create(
         to=contact,
         from_=tw_keys['MY_TWILIO_NUMBER'],
