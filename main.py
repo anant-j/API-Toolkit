2863b830b0f0542738402245aea233be65fc533c
Anant Jain <anant.j2409@gmail.com>
2019-12-30 18:35:48 -0500

    Fixing document formatting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#https://cloud.google.com/community/tutorials/building-flask-api-with-cloud-firestore-and-deploying-to-cloud-run
+# https://cloud.google.com/community/tutorials/building-flask-api-with-cloud-firestore-and-deploying-to-cloud-run
 
-from flask import Flask, request,redirect,send_from_directory
+from flask import Flask, request, redirect, send_from_directory
 from flask_cors import CORS
 from firebase_admin import credentials, firestore, initialize_app
 from twilio.twiml.messaging_response import MessagingResponse
@@ -9,8 +9,9 @@ import sms_handler
 import pushbullet
 import git
 
-Known_Users = dict({"2193543988": 'ANANT-WORK', "117193127": 'ANANT-PC', "4069111623":'ANANT-PHONE'})  #Users whose Unique Id is known
-Fallback = "https://www.anant-j.com" #Fallback original website
+Known_Users = dict({"2193543988": 'ANANT-WORK', "117193127": 'ANANT-PC',
+                    "4069111623": 'ANANT-PHONE'})  # Users whose Unique Id is known
+Fallback = "https://www.anant-j.com"  # Fallback original website
 Statuspage = "https://www.anant-j.com/api_status.html"
 Auth_Token = "QBLHnUhSdCzrh1DKXYDtR77gMsq4y6Ef"
 Auth_Host = "www.anant-j.com"
@@ -28,51 +29,52 @@ db = firestore.client()
 # Basic API Route
 @app.route('/')
 def redirected():
-  return redirect(Fallback, code=302)
+    return redirect(Fallback, code=302)
 
-#Load Favicon
+# Load Favicon
 @app.route('/favicon.ico')
 def favicon():
-  return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 # Health Check Route
 @app.route('/status')
 def health():
-  return ("UP", 200)
+    return ("UP", 200)
 
 # Core API to Add Data to Firestore + Push messages via Pushbullet
-@app.route('/api', methods=['POST']) #GET requests will be blocked
+@app.route('/api', methods=['POST'])  # GET requests will be blocked
 def add():
-  # https://scotch.io/bar-talk/processing-incoming-request-data-in-flask
+    # https://scotch.io/bar-talk/processing-incoming-request-data-in-flask
     req_data = request.get_json()
     Page = req_data['Page']
     Ip = req_data['Ip Address']
     Time = req_data['Date & Time']
     Fid = str(req_data['Fingerprint Id'])
 
-    #Check if User is already known
+    # Check if User is already known
     if(Fid in Known_Users):
-      Fid=Known_Users[Fid]
+        Fid = Known_Users[Fid]
     else:
-      if(req_data['Host']==Auth_Host):
-        pushbullet.send(req_data) # Send Notification Function Call
-    if(req_data['Host']==Auth_Host): # Hostname Verification to prevent spoofing
-      try:
-        db.collection(Page).document("UID: "+Fid).collection("IP: "+Ip).document(Time).set(req_data) #Add data to Firebase Firestore
-        return ("Sent", 200)
-      except Exception as e:
-        return (":( An Error Occured while sending data to Firebase:",{e})
-    else: #If user is unauthorized to call the api
-      return ("Unauthorized User",401)
+        if(req_data['Host'] == Auth_Host):
+            pushbullet.send(req_data)  # Send Notification Function Call
+    if(req_data['Host'] == Auth_Host):  # Hostname Verification to prevent spoofing
+        try:
+            db.collection(Page).document("UID: "+Fid).collection("IP: " +
+                                                                 Ip).document(Time).set(req_data)  # Add data to Firebase Firestore
+            return ("Sent", 200)
+        except Exception as e:
+            return (":( An Error Occured while sending data to Firebase:", {e})
+    else:  # If user is unauthorized to call the api
+        return ("Unauthorized User", 401)
 
-#Route to Delete All Pushbullet Notifications. # Route to Shut Down API. Uses 256-bit key encryption.
+# Route to Delete All Pushbullet Notifications. # Route to Shut Down API. Uses 256-bit key encryption.
 @app.route('/pbdel', methods=['GET'])
 def pbdelete():
-  AuthCode = request.args.get('auth')
-  if(AuthCode==Auth_Token):
-    return(pushbullet.delete())
-  else:
-    return ("Unauthorized User",401)
+    AuthCode = request.args.get('auth')
+    if(AuthCode == Auth_Token):
+        return(pushbullet.delete())
+    else:
+        return ("Unauthorized User", 401)
 
 # Route for SMS. Uses Twilio API
 @app.route("/sms", methods=["POST"])
@@ -80,12 +82,12 @@ def sms_reply():
     message_content = request.values.get('Body', None)
     contact = request.values.get('From', None)
     try:
-      res = sms_handler.send_sms(message_content, contact)
-      resp = MessagingResponse()
-      resp.message(res)
-      return ("SMS Message Sent", 200)
+        res = sms_handler.send_sms(message_content, contact)
+        resp = MessagingResponse()
+        resp.message(res)
+        return ("SMS Message Sent", 200)
     except Exception as e:
-      return ("An Error Occured while sending SMS", e)
+        return ("An Error Occured while sending SMS", e)
 
 # CI with GitHub
 @app.route('/update_server', methods=['POST'])
@@ -103,9 +105,9 @@ def webhook():
 # Handle Internal Server Errors
 @app.errorhandler(500)
 def e500(e):
-  return ("Internal Server Error", 500)
+    return ("Internal Server Error", 500)
 
 # If user enters wrong api link -> Redirect to main website
 @app.errorhandler(404)
 def e404(e):
-  return redirect(Fallback, code=302)
+    return redirect(Fallback, code=302)
