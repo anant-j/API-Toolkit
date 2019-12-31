f3b3d5e561efe1708c8dbf4c57aa5fa986f42da8
Anant Jain <anant.j2409@gmail.com>
2019-12-30 19:17:22 -0500

    Added Github security validation for webhook

---
+++ github_verify.py
@@ -0,0 +1,15 @@
+import hmac
+import hashlib
+my_directory = os.path.dirname(os.path.abspath(__file__))
+with open(my_directory+'/secrets/github_webook.json') as f:
+    github_key = json.load(f)
+webhook_key=github_key['key']
+
+def is_valid_signature(x_hub_signature, data):
+    # x_hub_signature and data are from the webhook payload
+    # private key is your webhook secret
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(webhook_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
