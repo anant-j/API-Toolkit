3fbe2eef430228e5f82acdadab85f0ca331dec10
Anant Jain <anant.j2409@gmail.com>
2020-01-11 00:59:09 -0500

    Update pushbullet.py

--- pushbullet.py
+++ pushbullet.py
@@ -25,7 +25,7 @@ def send_analytics(req):
 def send_form(formData):
     url = 'https://api.pushbullet.com/v2/pushes'
     content = {
-        "body": formData,
+        "body": str(formData),
         "title": "Someone sent you a message via contact form",
         "device_iden": DEVID,
         "type": "note"}
