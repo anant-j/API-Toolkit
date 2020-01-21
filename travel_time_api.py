2cfdbcea0291f23b550adc9b4369672d74efe41d
Anant Jain <anant.j2409@gmail.com>
2020-01-20 23:00:12 -0500

    Update travel_time_api.py

--- travel_time_api.py
+++ travel_time_api.py
@@ -84,6 +84,7 @@ def gowork():
 def eta(h,m):
     res = urlopen('http://just-the-time.appspot.com/')
     result = res.read().strip()
+    result_str = result.decode('utf-8')
     from_zone = tz.gettz('UTC')
     to_zone = tz.gettz('America/New_York')
     utc = datetime.strptime(result_str, '%Y-%m-%d %H:%M:%S')
