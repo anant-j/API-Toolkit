b21b685cd4619d750d403d6423e573aefe02570a
Anant Jain <anant.j2409@gmail.com>
2021-02-03 22:40:22 -0500

    Update test_deployment.py

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -1,26 +1,28 @@
 import requests
 import json
 
-deployment = requests.get('http://stagingapi.pythonanywhere.com/git')
-if deployment.status_code == 200:
-    dep_result = deployment.text.split(",")
-    branch = dep_result[0]
-    dep_hash = str(dep_result[1])
-else:
-    assert(False)
 
-try:
-    git = requests.get(
-        "https://api.github.com/repos/anant-j/API-Toolkit/git/refs/heads/" +
-        branch)
-except Exception:
-    git = requests.get(
-        "https://api.github.com/repos/anant-j/API-Toolkit/git/refs/heads/master")
+def test_staging_upload():
+    deployment = requests.get('http://stagingapi.pythonanywhere.com/git')
+    if deployment.status_code == 200:
+        dep_result = deployment.text.split(",")
+        branch = dep_result[0]
+        dep_hash = str(dep_result[1])
+    else:
+        assert(False)
 
-if git.status_code == 200:
-    res = json.loads(git.text)
-    sha = str(res["object"]["sha"])
-else:
-    assert(False)
+    try:
+        git = requests.get(
+            "https://api.github.com/repos/anant-j/API-Toolkit/git/refs/heads/" +
+            branch)
+    except Exception:
+        git = requests.get(
+            "https://api.github.com/repos/anant-j/API-Toolkit/git/refs/heads/master")
 
-assert(dep_hash == sha)
+    if git.status_code == 200:
+        res = json.loads(git.text)
+        sha = str(res["object"]["sha"])
+    else:
+        assert(False)
+
+    assert (dep_hash == sha), "test failed"
